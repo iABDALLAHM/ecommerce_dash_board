2c70856fa85676c961d572eb3eea70a5f7224a67
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2026-01-08 11:14:12 +0200

    add font family

--- lib/features/dashboard/presentation/views/widgets/dashboard_view_body.dart
+++ lib/features/dashboard/presentation/views/widgets/dashboard_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_dash_board/constants.dart';
+import 'package:ecommerce_dash_board/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class DashboardViewBody extends StatelessWidget {
@@ -5,10 +7,20 @@ class DashboardViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: CustomButton(text: "إضافة المنتج", onPressed: () {}),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:ecommerce_dash_board/core/function/on_generate_route.dart';
 import 'package:ecommerce_dash_board/features/dashboard/presentation/views/dashboard_view.dart';
+import 'package:ecommerce_dash_board/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const EcommerceDashboard());
@@ -12,9 +14,21 @@ class EcommerceDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: "Cairo",
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+      ),
       onGenerateRoute: onGenerateRoute,
       initialRoute: DashboardView.routeName,
       debugShowCheckedModeBanner: false,
+      locale: const Locale("ar"),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
     );
   }
 }
